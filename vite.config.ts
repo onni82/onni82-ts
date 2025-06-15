8d225f5ea56330cf6f47f1f61fabe4e81e0ecc54
onni82 <onni.bucht@me.com>
2025-06-15 04:25:55 +0200

    changed indentation

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/onni82-ts',
+	plugins: [react()],
+	base: '/onni82-ts',
 })
